1f7d6868dfe293b015a595cadc0b433c2eaab6a6
Doan Ngoc Thuong <45509523+ngthuongdoan@users.noreply.github.com>
2020-10-08 21:46:43 +0700

    Print Feature

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -2,33 +2,46 @@
   <div class="notification">
     <div class="buttons">
       <div
-        id="accountBtn"
-        class="button"
-        title="Change Information"
-        @click="changeInformation"
+          id="accountBtn"
+          class="button"
+          title="Change Information"
+          @click="changeInformation"
       >
         <img
-          alt="Change Information"
-          src="https://img.icons8.com/ios-glyphs/30/000000/guest-male.png"
+            alt="Change Information"
+            src="https://img.icons8.com/ios-glyphs/30/000000/guest-male.png"
         />
       </div>
       <div
-        v-if="!isTechnician"
-        id="addBtn"
-        class="button"
-        title="Add Technician"
-        @click="addNew"
+          v-if="!isTechnician"
+          id="addBtn"
+          class="button"
+          title="Add Technician"
+          @click="addNew"
       >
         <img
-          alt="Add Technician"
-          src="https://img.icons8.com/android/24/000000/plus.png"
+            alt="Add Technician"
+            src="https://img.icons8.com/android/24/000000/plus.png"
+        />
+      </div>
+      <div
+          v-if="!isTechnician"
+          id="printBtn"
+          class="button"
+          title="Print"
+          @click="print"
+      >
+        <img
+            alt="print"
+            src="https://img.icons8.com/ios-glyphs/50/000000/print.png"
+            width="16px"
         />
       </div>
       <div id="signOutBtn" class="button" title="Logout" @click="signOut">
         <img
-          alt="Logout"
-          src="https://img.icons8.com/metro/26/000000/export.png"
-          width="16px"
+            alt="Logout"
+            src="https://img.icons8.com/metro/26/000000/export.png"
+            width="16px"
         />
       </div>
     </div>
@@ -40,7 +53,11 @@ export default {
   methods: {
     signOut() {
       this.$store.dispatch("userModule/signOut");
-      this.$router.push("/login").catch(() => {});
+      this.$router.push("/login").catch(() => {
+      });
+    },
+    print() {
+      window.open("https://helpdesk-v2.herokuapp.com/v1/pdf","_blank");
     },
     changeInformation() {
       this.$emit("change-information");
@@ -52,7 +69,7 @@ export default {
   computed: {
     isTechnician() {
       return (
-        this.$store.getters["userModule/getUser"].data.role === "technician"
+          this.$store.getters["userModule/getUser"].data.role === "technician"
       );
     },
   },
@@ -81,6 +98,7 @@ export default {
 
 #signOutBtn,
 #addBtn,
+#printBtn,
 #accountBtn {
   width: 35px;
   height: 35px;
@@ -95,6 +113,7 @@ export default {
 }
 
 #addBtn,
+#printBtn,
 #accountBtn {
   background-color: white;
 
