2884d507ad1e70236eda6c5b90ec2178ea4d89fc
Doan Ngoc Thuong <45509523+ngthuongdoan@users.noreply.github.com>
2020-11-29 09:14:04 +0700

    change url

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -2,46 +2,46 @@
   <div class="notification">
     <div class="buttons">
       <div
-          id="accountBtn"
-          class="button"
-          title="Change Information"
-          @click="changeInformation"
+        id="accountBtn"
+        class="button"
+        title="Change Information"
+        @click="changeInformation"
       >
         <img
-            alt="Change Information"
-            src="https://img.icons8.com/ios-glyphs/30/000000/guest-male.png"
+          alt="Change Information"
+          src="https://img.icons8.com/ios-glyphs/30/000000/guest-male.png"
         />
       </div>
       <div
-          v-if="!isTechnician"
-          id="addBtn"
-          class="button"
-          title="Add Technician"
-          @click="addNew"
+        v-if="!isTechnician"
+        id="addBtn"
+        class="button"
+        title="Add Technician"
+        @click="addNew"
       >
         <img
-            alt="Add Technician"
-            src="https://img.icons8.com/android/24/000000/plus.png"
+          alt="Add Technician"
+          src="https://img.icons8.com/android/24/000000/plus.png"
         />
       </div>
       <div
-          v-if="!isTechnician"
-          id="printBtn"
-          class="button"
-          title="Print"
-          @click="print"
+        v-if="!isTechnician"
+        id="printBtn"
+        class="button"
+        title="Print"
+        @click="print"
       >
         <img
-            alt="print"
-            src="https://img.icons8.com/ios-glyphs/50/000000/print.png"
-            width="16px"
+          alt="print"
+          src="https://img.icons8.com/ios-glyphs/50/000000/print.png"
+          width="16px"
         />
       </div>
       <div id="signOutBtn" class="button" title="Logout" @click="signOut">
         <img
-            alt="Logout"
-            src="https://img.icons8.com/metro/26/000000/export.png"
-            width="16px"
+          alt="Logout"
+          src="https://img.icons8.com/metro/26/000000/export.png"
+          width="16px"
         />
       </div>
     </div>
@@ -53,11 +53,10 @@ export default {
   methods: {
     signOut() {
       this.$store.dispatch("userModule/signOut");
-      this.$router.push("/login").catch(() => {
-      });
+      this.$router.push("/login").catch(() => {});
     },
     print() {
-      window.open("https://helpdesk-v2.herokuapp.com/v1/pdf","_blank");
+      window.open("https://helpdesk-v2-demo.herokuapp.com/v1/pdf", "_blank");
     },
     changeInformation() {
       this.$emit("change-information");
@@ -69,7 +68,7 @@ export default {
   computed: {
     isTechnician() {
       return (
-          this.$store.getters["userModule/getUser"].data.role === "technician"
+        this.$store.getters["userModule/getUser"].data.role === "technician"
       );
     },
   },
